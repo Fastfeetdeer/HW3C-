6e06f8b3263113a495113511069cfe3ac004d1d6
e.demin <demsq@mail.ru>
2023-02-09 15:29:05 +0300

    complete task 3.3

--- HW_3.3/Program.cs
+++ HW_3.3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ДЗ 3.3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Инициализация программы");
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out int a);
+int b = 1;
+Console.WriteLine($"Выведение таблицы кубов от {b} до {a}");
+
+while(b <= a)
+{
+    System.Console.WriteLine(Math.Pow(b, 3));
+    b++;
+}
+Console.WriteLine("Программа завершила работу");
