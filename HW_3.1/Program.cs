f556fc232eaabec93d5786e05f60403cb28ef8f5
e.demin <demsq@mail.ru>
2023-02-09 15:08:20 +0300

    Complete task 3.1

--- HW_3.1/Program.cs
+++ HW_3.1/Program.cs
@@ -1,1 +1,27 @@
 ﻿// ДЗ 3.1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Инициализация программы");
+Console.Write("Введите число: ");
+
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+    int numberLengh=(int)Math.Log10(Math.Abs(number)) + 1;
+    if (numberLengh != 5 ) Console.WriteLine("Число не пятизначное");
+    int revNumber=0;
+    int doubleNumber=number;
+
+    for(int i=5; i != 0; i--)
+    {
+        revNumber=revNumber * 10;
+        revNumber=revNumber + (doubleNumber % 10);
+        doubleNumber=doubleNumber / 10;
+    }
+    
+    if (number != revNumber)
+    {
+    Console.WriteLine($"The entered {number} is not a palindrome");
+    }
+    else Console.WriteLine($"The entered  {number} is a palindrome");
+}
+else Console.WriteLine("Введено не целое число");
+
+Console.WriteLine("Программа завершила работу");
