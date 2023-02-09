28d6cab52fb5fa70e702465335b94ed471cf6ee2
e.demin <demsq@mail.ru>
2023-02-09 15:37:22 +0300

    edit task 3.3

--- HW_3.3/Program.cs
+++ HW_3.3/Program.cs
@@ -1,13 +1,21 @@
 ﻿// ДЗ 3.3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Console.WriteLine("Инициализация программы");
 Console.WriteLine("Введите число");
-int.TryParse(Console.ReadLine(), out int a);
-int b = 1;
-Console.WriteLine($"Выведение таблицы кубов от {b} до {a}");
-
-while(b <= a)
+if(int.TryParse(Console.ReadLine(), out int a))
 {
-    System.Console.WriteLine(Math.Pow(b, 3));
-    b++;
+
+
+    int b = 1;
+    Console.WriteLine($"Выведение таблицы кубов от {b} до {a}");
+
+    while(b <= a)
+    {
+        System.Console.WriteLine(Math.Pow(b, 3));
+        b++;
+    }
+}
+else
+{
+    Console.WriteLine("Введено не число");
 }
 Console.WriteLine("Программа завершила работу");
